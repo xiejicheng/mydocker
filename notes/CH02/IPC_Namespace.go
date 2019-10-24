046b3a5aabed405585f6a33221866c252afbf33a
xiejicheng <31449070+xiejicheng@users.noreply.github.com>
2019-10-25 01:08:53 +0800

    Update IPC_Namespace.go

--- notes/CH02/IPC_Namespace.go
+++ notes/CH02/IPC_Namespace.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	cmd := exec.Command("sh")
-	cmd.SysProcAttr = &syscall.SysProcAttr {
+	cmd.SysProcAttr = &syscall.SysProcAttr {  // 添加了CLONE_NEWIPC参数
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC,
 	}
 	cmd.Stdin = os.Stdin
